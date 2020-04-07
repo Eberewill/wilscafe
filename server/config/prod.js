3cef10ca68eb8d6c64226693751fd77dc43a980b
Willex <eberewill@gmail.com>
2020-04-07 13:41:13 +0100

    final

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:'mongodb+srv://willex:<chemistry@123>@cluster0-wt8d3.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://willex:chemistry@123@cluster0-wt8d3.mongodb.net/test?retryWrites=true&w=majority'
 }
